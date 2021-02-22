a2c1a9f9be1aea81e540715c0b1ced990aa97c38
aleksey.dobrovolsky <aleksey.dobrovolsky@elliemae.com>
2021-02-22 21:23:06 +0300

    feat: cats, carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (!isNumeric(sampleActivity)) {
+    return false;
+  }
+  let result = Math.ceil(
+    (Math.log(MODERN_ACTIVITY / sampleActivity) * HALF_LIFE_PERIOD) / 0.693
+  );
+  return Number.isInteger(result) && result >= 0 ? result : false;
 };
+
+function isNumeric(str) {
+  if (typeof str != "string") return false;
+  return !isNaN(str) && !isNaN(parseFloat(str));
+}
