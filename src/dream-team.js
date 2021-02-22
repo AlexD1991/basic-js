0df707f206cfebd497329e92e832e3de37ea4794
aleksey.dobrovolsky <aleksey.dobrovolsky@elliemae.com>
2021-02-22 21:45:48 +0300

    feat: createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  return members
+    .filter((name) => isString(name))
+    .map((name) => name.trim())
+    .map((name) => name.toUpperCase())
+    .sort()
+    .map((name) => name[0])
+    .join("");
 };
+
+function isString(str) {
+  return typeof str === "string";
+}
