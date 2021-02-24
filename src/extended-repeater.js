2a670c0b497f784ea4106297a4b178de07975446
aleksey.dobrovolsky <aleksey.dobrovolsky@elliemae.com>
2021-02-24 21:44:02 +0300

    feat: extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let resultStr = "";
+
+  let map = new Map(Object.entries(options));
+  str = str + "";
+  let repeatTimes = Number.isInteger(map.get("repeatTimes"))
+    ? map.get("repeatTimes")
+    : 1;
+  let separator = map.get("separator") ? map.get("separator") : "+";
+  let addition =
+    map.get("addition") ||
+    map.get("addition") === false ||
+    map.get("addition") === null
+      ? map.get("addition") + ""
+      : "";
+  let additionRepeatTimes = Number.isInteger(map.get("additionRepeatTimes"))
+    ? map.get("additionRepeatTimes")
+    : 1;
+  let additionSeparator = map.get("additionSeparator")
+    ? map.get("additionSeparator")
+    : "|";
+
+  for (let mainCount = 0; mainCount < repeatTimes; mainCount++) {
+    resultStr += str + "";
+    for (
+      let additionalCount = 0;
+      additionalCount < additionRepeatTimes;
+      additionalCount++
+    ) {
+      resultStr +=
+        additionalCount === additionRepeatTimes - 1
+          ? addition
+          : addition + additionSeparator;
+    }
+    resultStr += mainCount === repeatTimes - 1 ? "" : separator;
+  }
+
+  return resultStr;
 };
-  
